3b4f41229b2986317fe52e779fe270e17782eee0
Ivan Branimir Skoric <ib.skoric@outlook.com>
2022-02-06 14:30:28 +0000

    Add functinality to get categories dynamically from the data file and display buttons based on the unique categories that exist

--- src/Categories.js
+++ src/Categories.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Categories = ({ filterItems }) => {
+const Categories = ({ filterItems, categories }) => {
   return (
       <div className="btn-container">
-        <button className='filter-btn' onClick={() => filterItems('all')}>All</button>
-        <button className='filter-btn' onClick={() => filterItems('breakfast')}>Breakfast</button>
-        <button className='filter-btn' onClick={() => filterItems('lunch')}>Lunch</button>
-        <button className='filter-btn' onClick={() => filterItems('shakes')}>Shakes</button>
+        {categories.map((category) => {
+          return(
+            <button className='filter-btn' onClick={() => filterItems(`${category}`)}>{category}</button>
+          )
+        })}
       </div>
     );
 };
